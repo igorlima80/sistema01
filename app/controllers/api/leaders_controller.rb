9e1d35dee25de87aaf9c688cf9dea0ab297e3a30
IgorLima <f.igor.lima@gmail.com>
2020-09-06 16:26:38 -0300

    forçando a entrada com um cpf no codigo

--- app/controllers/api/leaders_controller.rb
+++ app/controllers/api/leaders_controller.rb
@@ -3,7 +3,7 @@ class Api::LeadersController < Api::ApplicationController
 
   def login
     @leader = {}
-    @leader = Leader.where(cpf: params[:cpf]).first
+    @leader = Leader.where(cpf: "81589123387").first
     
 
     if @leader
